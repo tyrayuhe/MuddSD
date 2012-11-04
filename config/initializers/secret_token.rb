d742c1084b2d22e2b63eb6b6ac6494ea56034af6
rails -v <pc@pc-PC.(none)>
2012-11-04 14:19:36 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MuddSD::Application.config.secret_token = '4e5a840f1157a39e1ccb7a8d569ef3991c4be7facc5e335776c3c7e6fb827abde4e70553868135900d6ec91a904ba595f83eb50810304cff2a538578357f5b78'
